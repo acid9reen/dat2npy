a62b2a782ff787e6558f29fe7602c314f6c23636
Ruslan Smirnov <smirnov_ruslan@outlook.com>
2023-07-01 12:23:27 +0300

    Add option to downsample signal

--- src/dat2npy/cli/convert_file.py
+++ src/dat2npy/cli/convert_file.py
@@ -2,12 +2,15 @@ import argparse
 from pathlib import Path
 
 import numpy as np
+from scipy.signal import decimate
 
 from dat2npy.dat_utils import read_dat_file
+from dat2npy.types import Hz
 
 
 class FileConverterNamespace(argparse.Namespace):
     filepath: Path
+    target_frequency: Hz | None = None
 
 
 def parse_args() -> FileConverterNamespace:
@@ -25,6 +28,16 @@ def parse_args() -> FileConverterNamespace:
         help="Path to .dat file to convert to .npy",
     )
 
+    parser.add_argument(
+        "-f",
+        "--target_frequency",
+        type=Hz,
+        help=(
+            "Target frequency for output signal, "
+            "must be less than frequency in original signal"
+        ),
+    )
+
     return parser.parse_args(namespace=FileConverterNamespace())
 
 
@@ -36,6 +49,15 @@ def main() -> int:
     output_filename = args.filepath.stem + ".npy"
     output_filepath = args.filepath.parent / output_filename
 
-    np.save(output_filepath, dat_file.signal)
+    signal_to_save = (
+        dat_file.signal if args.target_frequency is None
+        else decimate(
+            dat_file.signal,
+            q=round(dat_file.frequency/args.target_frequency),
+            axis=1,
+        )
+    )
+
+    np.save(output_filepath, signal_to_save)
 
     return 0
